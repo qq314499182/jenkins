c3f7c7aa74827f91ae3647effc45d8ebb2311df9
zhaoke <zhaoke2@lenovo.com>
2020-10-19 13:48:21 +0800

    add test web

---
+++ src/main/java/com/tct/iids/web/JenkinsWeb.java
@@ -0,0 +1,18 @@
+package com.tct.iids.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description
+ * @Author zhaoke
+ * @Date 2020/10/19 13:46
+ */
+@RestController
+public class JenkinsWeb {
+
+    @GetMapping("test")
+    public String test(){
+        return "hello jenkins";
+    }
+}
